564ce1f0531f6c6da2b28b28ae7ad84c028c77dc
develowlper <d.schulz@hello-sunshine.dev>
2025-10-02 16:10:42 +0200

    feat: meta data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
+export const metadata = {
+  title: "Karl Eule",
+  description: "Portfolio of Karl Eule",
+};
+
 export default function Home() {
   return (
     <div className="h-screen flex items-center justify-center text-3xl bold text-primary">
